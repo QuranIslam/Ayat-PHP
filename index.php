21109146f717b42f3c572da39452e07e08e2cbd3
QuranIslam <47284416+QuranIslam@users.noreply.github.com>
2023-01-18 22:59:16 +0200

    Mobile Fix

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
-
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
+$mobile_agents = array(
+    'Android', 'AvantGo', 'BlackBerry', 'DoCoMo', 'Fennec', 'iPod', 'iPhone', 'iPad',
+    'J2ME', 'MIDP', 'NetFront', 'Nokia', 'Opera Mini', 'Opera Mobi', 'PalmOS', 'PalmSource',
+    'portalmmm', 'Plucker', 'ReqwirelessWeb', 'SonyEricsson', 'Symbian', 'UP.Browser',
+    'webOS', 'Windows CE', 'Windows Phone', 'Xiino'
+);
 
-if (preg_match('/(tablet|ipad|playbook)|(android(?!.*(mobi|opera mini)))/i', $user_agent)) {
-    include 'm.html';
+$is_mobile = false;
+
+foreach ($mobile_agents as $agent) {
+    if (stripos($user_agent, $agent) !== false) {
+        $is_mobile = true;
+        break;
+    }
 }
-else {
+
+if ($is_mobile) {
+    include 'm.html';
+} else {
     include 'index.html';
 }
 
